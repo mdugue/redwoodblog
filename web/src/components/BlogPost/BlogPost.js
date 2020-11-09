2983c4785b4b7c20031d634de63db6e3068b25cd
Manuel Dugué <manuel.dugue@comvel.de>
2020-11-09 12:54:14 +0100

    route params

---
+++ web/src/components/BlogPost/BlogPost.js
@@ -0,0 +1,15 @@
+import { Link, routes } from '@redwoodjs/router'
+
+const BlogPost = ({ post }) => {
+  return (
+    <article>
+      <header>
+        <h2>
+          <Link to={routes.blogPost({ id: post.id })}>{post.title}</Link>
+        </h2>
+      </header>
+    </article>
+  )
+}
+
+export default BlogPost
